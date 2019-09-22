4f6cad56dba822f9ced924e8965d119cfae42174
Othman <otma.ctos.asslan@hotmail.com>
2019-09-22 14:55:17 +0300

    ttttttttttt

--- app/src/main/java/com/example/talamenu2/MainActivity.java
+++ app/src/main/java/com/example/talamenu2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.talamenu2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater=getMenuInflater();
+        inflater.inflate(R.menu.my)
     }
 }
